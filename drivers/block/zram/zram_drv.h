faabbee13c7112aa0e6af95163151f4811b43ee6
Minchan Kim <minchan@kernel.org>
2018-12-28 00:36:54 -0800

    UPSTREAM: zram: writeback throttle
    If there are lots of write IO with flash device, it could have a
    wearout problem of storage. To overcome the problem, admin needs
    to design write limitation to guarantee flash health
    for entire product life.
    
    This patch creates a new knob "writeback_limit" for zram.
    
    writeback_limit's default value is 0 so that it doesn't limit
    any writeback. If admin want to measure writeback count in a
    certain period, he could know it via /sys/block/zram0/bd_stat's
    3rd column.
    
    If admin want to limit writeback as per-day 400M, he could do it
    like below.
    
    	MB_SHIFT=20
    	4K_SHIFT=12
    	echo $((400<<MB_SHIFT>>4K_SHIFT)) > \
    		/sys/block/zram0/writeback_limit.
    
    If admin want to allow further write again, he could do it like below
    
    	echo 0 > /sys/block/zram0/writeback_limit
    
    If admin want to see remaining writeback budget,
    
    	cat /sys/block/zram0/writeback_limit
    
    The writeback_limit count will reset whenever you reset zram (e.g., system
    reboot, echo 1 > /sys/block/zramX/reset) so keeping how many of writeback
    happened until you reset the zram to allocate extra writeback budget in
    next setting is user's job.
    
    [minchan@kernel.org: v4]
      Link: http://lkml.kernel.org/r/20181203024045.153534-8-minchan@kernel.org
    Link: http://lkml.kernel.org/r/20181127055429.251614-8-minchan@kernel.org
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Joey Pabalinas <joeypabalinas@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit bb416d18b850faaa44bd3bb67c9728922c3cce98)
    
    Bug: 117683045
    Change-Id: Ib82f76d1cecaf251e75584f8140c9b4949cd27b2
    Signed-off-by: Srinivas Paladugu <srnvs@google.com>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -85,6 +85,7 @@ struct zram_stats {
 	atomic64_t bd_count;		/* no. of pages in backing device */
 	atomic64_t bd_reads;		/* no. of reads from backing device */
 	atomic64_t bd_writes;		/* no. of writes from backing device */
+	atomic64_t bd_wb_limit;		/* writeback limit of backing device */
 #endif
 };
 
@@ -112,6 +113,7 @@ struct zram {
 	 */
 	bool claim; /* Protected by bdev->bd_mutex */
 	struct file *backing_dev;
+	bool stop_writeback;
 #ifdef CONFIG_ZRAM_WRITEBACK
 	struct block_device *bdev;
 	unsigned int old_block_size;
