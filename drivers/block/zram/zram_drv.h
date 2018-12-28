ad3249920f793a055f45c1375aa9d8a028a3d16b
Minchan Kim <minchan@kernel.org>
2018-12-28 00:36:44 -0800

    UPSTREAM: zram: introduce ZRAM_IDLE flag
    To support idle page writeback with upcoming patches, this patch
    introduces a new ZRAM_IDLE flag.
    
    Userspace can mark zram slots as "idle" via
    	"echo all > /sys/block/zramX/idle"
    which marks every allocated zram slot as ZRAM_IDLE.
    User could see it by /sys/kernel/debug/zram/zram0/block_state.
    
              300    75.033841 ...i
              301    63.806904 s..i
              302    63.806919 ..hi
    
    Once there is IO for the slot, the mark will be disappeared.
    
    	  300    75.033841 ...
              301    63.806904 s..i
              302    63.806919 ..hi
    
    Therefore, 300th block is idle zpage. With this feature,
    user can how many zram has idle pages which are waste of memory.
    
    Link: http://lkml.kernel.org/r/20181127055429.251614-5-minchan@kernel.org
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Reviewed-by: Joey Pabalinas <joeypabalinas@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit e82592c4fd7eafe8dec12a70436e93e3afb28556)
    
    Bug: 117683045
    Change-Id: Ie9545751de4cfce89885989cc6ee35a50691ff4e
    Signed-off-by: Srinivas Paladugu <srnvs@google.com>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -50,6 +50,7 @@ enum zram_pageflags {
 	ZRAM_WB,	/* page is stored on backing_device */
 	ZRAM_UNDER_WB,	/* page is under writeback */
 	ZRAM_HUGE,	/* Incompressible page */
+	ZRAM_IDLE,	/* not accessed page since last idle marking */
 
 	__NR_ZRAM_PAGEFLAGS,
 };
